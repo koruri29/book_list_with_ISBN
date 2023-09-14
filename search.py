3a82e2b3a8f7747a9ee56c3576912cb154433776
koruri29 <koruri821@yahoo.co.jp>
2023-09-14 11:21:26 +0900

    検索サイトを紀伊国屋に変更、リンクではなくISBNを出力するように変更

--- search.py
+++ search.py
@@ -8,47 +8,46 @@ sys.path.append("lib.bs4") # BS4エラー対策
 def getSearchResults(url, *keywords):
     encoded = encodeKeywords(keywords)
     url += encoded
-    #print(url)
     
     response = requests.get(url)
     
-    response.encoding = response.apparent_encoding
-    filename = "download.txt"
-    f = open(filename, mode="w")
-    with open(filename, mode="w") as f:
-        f.write(response.text)    
-    soup = BeautifulSoup(response.content, "html.parser")
+    # 確認出力用
+    # response.encoding = response.apparent_encoding
+    # filename = "download.txt"
+    # f = open(filename, mode="w")
+    # with open(filename, mode="w") as f:
+    #     f.write(response.text)    
     
-    #検索にヒットしなかった場合
-    not_found = soup.find("div", attrs = {"class": "noHitBox"})
-    if not_found != None:
-        return print("検索結果が0件です。")
     
     #書籍タイトル取得
-    # title_parents = soup.find_all("p", attres = {"class": "itemttl"})
-    title_parents = soup.select("p.itemttl > a")
-    # print(title_parents)
-    titles = []
-    count = 0
-    for title_parent in title_parents:
-        titles.append(title_parent.contents[0])
-        print(titles[count])
-        count += 1
+    soup = BeautifulSoup(response.content, "html.parser")
+    book_items = soup.select("h3.heightLine-2 > a")
+    
+    if book_items == None:
+        print("検索結果が0件です。")
+        return
+    
+    items = {}
+    for book_item in book_items:
+        book_title = book_item.contents[0]
+        book_link = urllib.parse.urljoin(url, book_item.get("href"))
+        isbn = book_link[38:]
+        items[book_title] = isbn
+    return items
     
 
 def encodeKeywords(*keywords):
-    double_encoded = ""
+    encoded = ""
     for keyword in keywords:
         if (type(keyword) != "<class 'str'>"): # 文字列型か判定、変換
             keyword = str(keyword)
-        encoded = urllib.parse.quote(keyword[3:-4], '/', encoding='shift_jis') # 引数1は(['キーワード'],)の形から整形している
-        double_encoded = urllib.parse.quote(encoded, '/', encoding='shift_jis')
-    return double_encoded
+        encoded += urllib.parse.quote(keyword[3:-4], '/', encoding='utf-8') # 引数1は(['キーワード'],)の形から整形している
+        encoded += "%E3%80%80" # 半角スペースのエンコーディング
+    return encoded
 
 
 # メインルーチン
-# url = "https://www.bookoffonline.co.jp/"
-url = "https://www.bookoffonline.co.jp/display/L001,st=a,q="
+url = "https://www.kinokuniya.co.jp/disp/CSfDispListPage_001.jsp?qs=true&ptk=01&q="
 # keywords = ["オブジェクト指向超入門"]
 
 print("本のタイトルを入力してください。")
@@ -57,4 +56,9 @@ keywords = keywords.replace("　", " ")
 keywords = keywords.split()
 
 
-getSearchResults(url, keywords)
+items = getSearchResults(url, keywords)
+
+if items != None:
+    for k in items:
+        print(k)
+        print(items[k])
